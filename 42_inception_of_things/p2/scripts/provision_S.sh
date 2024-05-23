c968492d51429d3ad5dd959e5ba41ebb5dbedbad
nicob42 <100759345+nicob42@users.noreply.github.com>
2024-05-23 10:24:47 +0200

    Add files via upload

--- 42_inception_of_things/p2/scripts/provision_S.sh
+++ 42_inception_of_things/p2/scripts/provision_S.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 GREEN="\033[32m"
 RED="\033[31m"
 RESET="\033[0m"
 
 echo "${GREEN}[INFO]  Installing k3s on server node (ip: $1) ===================>>>>>>>>//////${RESET}"
 
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 "
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1 --bind-address=$1 --advertise-address=$1"
 
 echo "${GREEN}[INFO]  ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC ===================>>>>>>>>//////${RESET}"
 
@@ -16,7 +17,8 @@ echo "${GREEN}[INFO]  Doing some sleep to wait for k3s to be ready =============
 
 sleep 10
 
-sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/scripts/
+echo "${GREEN}[INFO]  Applying Kubernetes manifests ===================>>>>>>>>//////${RESET}"
+sudo k3s kubectl apply -f /goinfre/nbechard/42_inception_of_things/p2/scripts/kubernetes_manifests/
 
 echo "${GREEN}[INFO]  Successfully installed k3s on server node ===================>>>>>>>>//////${RESET}"
 
